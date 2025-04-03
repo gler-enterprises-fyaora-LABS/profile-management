be13851a3f045eab40a1a1b7ed13b8e977cdd26c
Ryan Ribeiro <ryan.r@fyaora.com>
2025-04-03 17:38:59 +0100

    Modify entity

--- src/main/java/com/fyaora/profilemanagement/profileservice/model/db/entity/UserType.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/model/db/entity/UserType.java
@@ -1,13 +1,15 @@
 package com.fyaora.profilemanagement.profileservice.model.db.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "user_type")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 public class UserType {
 
     @Id
@@ -15,43 +17,11 @@ public class UserType {
     private Integer did; // Primary Key
 
     @Column(nullable = false)
-    private Integer type; // 1 = Registration Service Provider, 2 = Customer Provider
+    private int type;
 
     @Column(nullable = false, length = 50)
     private String description;
 
     @Column(nullable = false)
     private Boolean enabled;
-
-    public Integer getDid() {
-        return did;
-    }
-
-    public void setDid(Integer did) {
-        this.did = did;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
 }
