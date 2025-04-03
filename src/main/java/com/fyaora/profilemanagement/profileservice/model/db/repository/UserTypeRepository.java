c910d852a31d12dbac95ca4744f6fe47e51bf849
Ryan Ribeiro <ryan.r@fyaora.com>
2025-04-03 17:33:22 +0100

    Remove @Repository

--- src/main/java/com/fyaora/profilemanagement/profileservice/model/db/repository/UserTypeRepository.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/model/db/repository/UserTypeRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface UserTypeRepository extends JpaRepository<UserType, Integer> {
 
     Optional<UserType> findByType(Integer type);
