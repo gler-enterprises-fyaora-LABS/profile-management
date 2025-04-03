24ae492a21d9652fc57786f746302fcb79894de3
Ryan Ribeiro <ryan.r@fyaora.com>
2025-04-03 17:29:45 +0100

    Add @JsonProperty

--- src/main/java/com/fyaora/profilemanagement/profileservice/dto/UserTypeDTO.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/dto/UserTypeDTO.java
@@ -1,18 +1,20 @@
 package com.fyaora.profilemanagement.profileservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserTypeDTO {
+    @JsonProperty("type")
     private Integer type;
+    @JsonProperty("description")
     private String description;
+    @JsonProperty("enabled")
     private Boolean enabled;
-
-    public UserTypeDTO(Integer type, String description, Boolean enabled) {
-        this.type = type;
-        this.description = description;
-        this.enabled = enabled;
-    }
 }
