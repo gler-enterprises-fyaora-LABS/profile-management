76636b35243e0535f9244c39094ccfc23b74533c
Ryan Ribeiro <ryan.r@fyaora.com>
2025-04-03 22:03:01 +0100

    Change findByType parameter type to UserTypeEnum

--- src/main/java/com/fyaora/profilemanagement/profileservice/model/db/repository/UserTypeRepository.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/model/db/repository/UserTypeRepository.java
@@ -1,11 +1,12 @@
 package com.fyaora.profilemanagement.profileservice.model.db.repository;
 
 import com.fyaora.profilemanagement.profileservice.model.db.entity.UserType;
+import com.fyaora.profilemanagement.profileservice.model.db.entity.UserTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 public interface UserTypeRepository extends JpaRepository<UserType, Integer> {
 
-    Optional<UserType> findByType(Integer type);
+    Optional<UserType> findByType(UserTypeEnum type);
 }
