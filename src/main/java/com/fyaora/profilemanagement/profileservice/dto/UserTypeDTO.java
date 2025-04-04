491974fad0d13a08bfdf6caa90ec0df25094a5dc
Ryan Ribeiro <ryan.r@fyaora.com>
2025-04-04 07:43:38 +0100

    Include id UserTypeDTO

--- src/main/java/com/fyaora/profilemanagement/profileservice/dto/UserTypeDTO.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/dto/UserTypeDTO.java
@@ -1,6 +1,7 @@
 package com.fyaora.profilemanagement.profileservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fyaora.profilemanagement.profileservice.model.db.entity.UserTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserTypeDTO {
+    @JsonProperty("id")
+    private Integer id;
     @JsonProperty("type")
-    private Integer type;
+    private UserTypeEnum type;
     @JsonProperty("description")
     private String description;
     @JsonProperty("enabled")
