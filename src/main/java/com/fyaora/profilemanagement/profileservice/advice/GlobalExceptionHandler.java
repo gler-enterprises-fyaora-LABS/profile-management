3e01b7bf64ffb4faad6dbfa31879227685f108c4
Ryan Ribeiro <ryan.r@fyaora.com>
2025-04-21 14:43:17 +0100

    Change use HttpStatus.NOT_FOUND and return a MessageDTO

--- src/main/java/com/fyaora/profilemanagement/profileservice/advice/GlobalExceptionHandler.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/advice/GlobalExceptionHandler.java
@@ -1,15 +1,29 @@
 package com.fyaora.profilemanagement.profileservice.advice;
 
+import com.fyaora.profilemanagement.profileservice.dto.MessageDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserTypeNotFoundException.class)
-    public ResponseEntity<?> handleUserTypeNotFoundException(Exception ex){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<?> handleUserTypeNotFoundException(Exception ex, WebRequest webRequest) {
+        MessageDTO messageDTO = getMessageDTO(HttpStatus.NOT_FOUND, ex.getMessage(), webRequest);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(messageDTO);
+    }
+
+    private MessageDTO getMessageDTO(HttpStatus status, String message, WebRequest webRequest) {
+        MessageDTO messageDTO = new MessageDTO();
+        messageDTO.setTimestamp(LocalDateTime.now());
+        messageDTO.setStatus(status.value());
+        messageDTO.setError(status.getReasonPhrase());
+        messageDTO.setMessage(message);
+        messageDTO.setPath(webRequest.getDescription(false).replace("uri=", ""));
+        return messageDTO;
     }
 }
