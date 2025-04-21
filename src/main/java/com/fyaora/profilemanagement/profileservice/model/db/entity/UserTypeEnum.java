a6a70e7f0532f95281e2f10c96761d3a2cb556bc
Ryan Ribeiro <ryan.r@fyaora.com>
2025-04-21 13:13:17 +0100

    Simplify the user types

--- src/main/java/com/fyaora/profilemanagement/profileservice/model/db/entity/UserTypeEnum.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/model/db/entity/UserTypeEnum.java
@@ -1,6 +1,6 @@
 package com.fyaora.profilemanagement.profileservice.model.db.entity;
 
 public enum UserTypeEnum {
-    REGISTRATION_SERVICE_PROVIDER,
-    CUSTOMER_PROVIDER;
+    SERVICE_PROVIDER,
+    CUSTOMER;
 }
