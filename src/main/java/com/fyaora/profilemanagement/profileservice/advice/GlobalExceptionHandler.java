4322075cf652ca05320a5a0897b371d45ce04740
Ryan Ribeiro <ryan.r@fyaora.com>
2025-05-25 21:31:46 +0100

    Add handleHttpMessageNotReadable

--- src/main/java/com/fyaora/profilemanagement/profileservice/advice/GlobalExceptionHandler.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/advice/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.fyaora.profilemanagement.profileservice.advice;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fyaora.profilemanagement.profileservice.dto.MessageDTO;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -17,6 +20,21 @@ public class GlobalExceptionHandler {
     public ResponseEntity<MessageDTO> handleUserTypeNotFoundException(UserTypeNotFoundException ex, WebRequest webRequest) {
         MessageDTO messageDTO = getMessageDTO(HttpStatus.NOT_FOUND, ex.getMessage(), webRequest);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(messageDTO);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<MessageDTO> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, WebRequest request) {
+        String message = "Bad request";
+
+        // If the root cause is enum conversion failure
+        if (ex.getCause() instanceof InvalidFormatException invalidEx &&
+                invalidEx.getTargetType() != null &&
+                invalidEx.getTargetType().isEnum()) {
+            message = "Invalid type. It should be CUSTOMER, SERVICE_PROVIDER, INDIVIDUAL or BUSINESS";
+        }
+
+        MessageDTO messageDTO = getMessageDTO(HttpStatus.BAD_REQUEST, message, request);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageDTO);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
@@ -36,6 +54,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<MessageDTO> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest webRequest) {
         MessageDTO messageDTO = getMessageDTO(HttpStatus.BAD_REQUEST, ex.getMessage(), webRequest);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageDTO);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<MessageDTO> handleConstraintViolationException(ConstraintViolationException ex, WebRequest webRequest) {
+        String msg = "Invalid type. It should be CUSTOMER, SERVICE_PROVIDER, INDIVIDUAL or BUSINESS";
+        MessageDTO messageDTO = getMessageDTO(HttpStatus.BAD_REQUEST, msg, webRequest);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageDTO);
     }
 
