706b40931fd52748a6fbe70f5c4f2722d03cbcb8
Ryan Ribeiro <ryan.r@fyaora.com>
2025-07-04 18:25:00 +0100

    Except id, rest of all other attributes should be MANDATORY.

--- src/main/java/com/fyaora/profilemanagement/profileservice/dto/UserTypeDTO.java
+++ src/main/java/com/fyaora/profilemanagement/profileservice/dto/UserTypeDTO.java
@@ -1,6 +1,8 @@
 package com.fyaora.profilemanagement.profileservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,16 @@ import lombok.Setter;
 public class UserTypeDTO {
     @JsonProperty("id")
     private Integer id;
+
     @JsonProperty("type")
+    @NotNull(message = "user.type.not.null")
     private UserTypeEnum type;
+
     @JsonProperty("description")
+    @NotBlank(message = "user.description.not.blank")
     private String description;
+
     @JsonProperty("enabled")
-    private Boolean enabled;
+    @NotNull(message = "user.enabled.not.null")
+    private boolean enabled;
 }
